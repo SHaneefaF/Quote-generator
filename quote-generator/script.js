6f1ef339dbbfd147beb711a2d02b4e91ba942cc9
unknown <syedahaneefafatima@gmail.com>
2021-02-20 15:34:50 +0530

    initial commit

---
+++ quote-generator/script.js
@@ -0,0 +1,76 @@
+const quoteContainer =  document.getElementById('quote-container');
+const quoteText =  document.getElementById('quote');
+const authorText=  document.getElementById('author');
+const twitterBtn =  document.getElementById('twitter');
+const newQuoteBtn =  document.getElementById('new-quote');
+const loader= document.getElementById('loader');
+
+//Show Loading spinner
+function showLoadingSpinner() {
+    loader.hidden=false;
+    quoteContainer.hidden=true;
+}
+
+//Hide Loading spinner
+function removeLoadingSpinner(){
+    if(!loader.hidden){
+        quoteContainer.hidden=false;
+        loader.hidden=true;
+    }
+}
+
+
+//Get Quote From API
+async function getQuote(){
+    showLoadingSpinner();
+    const proxyUrl= 'https://damp-garden-34644.herokuapp.com/';
+    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    try {
+        const response= await fetch(proxyUrl + apiUrl);
+        const data = await response.json();
+
+        //If Author is blank,add Unknown.
+        if(data.quoteAuthor== ''){
+            authorText.innerText='Unknown';
+        }else{
+            authorText.innerText=data.quoteAuthor;
+        }
+        //console.log(data);
+
+        //Reduce font size for long quotes.
+        if(data.quoteText.length>120){
+            quoteText.classList.add('long-quote');
+        }else{
+            quoteText.classList.remove('long-quote');
+        }
+
+        quoteText.innerText=data.quoteText;
+
+        //Stop loader,show quote
+        removeLoadingSpinner();
+    
+
+        
+    } catch (error) {
+        getQuote();
+    
+        console.log('Whoops, No quote', error);
+        
+    }
+}
+
+//Tweet the quote
+ function tweetQuote(){
+     const quote= quoteText.innerText;
+     const author=authorText.innerText;
+     const twitterUrl=`https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+     window.open(twitterUrl, '_blank');
+ }
+
+ //Even Listeners for buttons
+
+ newQuoteBtn.addEventListener('click', getQuote);
+ twitterBtn.addEventListener('click',tweetQuote);
+
+//On Load Of Page
+getQuote();
